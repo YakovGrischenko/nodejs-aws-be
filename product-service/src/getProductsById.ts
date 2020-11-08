d785a2f038353a8a9af6ee9e01e6905c2eb15221
Yakov Grischenko <coding@mail.ru>
2020-11-08 20:47:07 +0700

    feat: getProductsList and getProductsById receive products from RDS

--- product-service/src/getProductsById.ts
+++ product-service/src/getProductsById.ts
@@ -1,67 +1,75 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
-import productList from "../productList.json";
-import axios from 'axios'
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import { Client } from "pg";
 
-// function is for async/awit try/catch example
-const getWeatherInfo = async (productId) => {
-  
-  const OPEN_WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
-  let apiKey = '2b5437d79044ea0ae5c424019c101372'
-  let cityName = productId || 'Barnaul'
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false, // to avoid warring in this example
+  },
+  connectionTimeoutMillis: 5000, // time in millisecond for termination of the database query
+};
 
-  if(productId === 'test_catch'){
-    apiKey = 'mock_unactive_api_key'
-  }
-
-  try{
-    const { data } =  await axios.get(`${OPEN_WEATHER_URL}?q=${cityName}&appid=${apiKey}`) 
-    return data;
-  } catch (error) {
-    return {
-      message: 'Unable to get weather info'
-    }
-  }
-}
-
-export const getProductsById: APIGatewayProxyHandler = async (event, _context) => {
-  
+export const getProductsById: APIGatewayProxyHandler = async (
+  event,
+  _context
+) => {
   let statusCode = 404;
-  let result = {  }
-  
+  let result = {};
+  const client = new Client(dbOptions);
+
   try {
-    
-    const { pathParameters:{productId}} = event;
-    
-    //  needed as awit using example
-    const  weatherData = await getWeatherInfo(productId)
+    await client.connect();
 
-    const product = productList.find(product => product.id === productId)
+    const {
+      pathParameters: { productId },
+    } = event;
 
-    if(product){
-      statusCode = 200;
-      result = product;
-    }
-    else{
-      result = {
-        message: 'Product not found'
+    try {
+      const text =
+        "select p.*, s.count from products as p inner join stocks as s on p.id = s.product_id where p.id = $1";
+
+      const {
+        rows: [product],
+      } = await client.query(text, [productId]);
+
+      if (product) {
+        statusCode = 200;
+        result = product;
+      } else {
+        statusCode = 404;
+        result = {
+          message: "Product not found",
+        };
       }
-    }  
-  } catch( error ) {
+    } catch (error) {
+      statusCode = 400;
+      result = {
+        message: "Product UUID format is invalid.",
+      };
+    }
+  } catch (error) {
     statusCode = 500;
     result = {
-      message: 'internal server error'
-    }
+      message: "internal server error",
+    };
+  } finally {
+    client.end();
   }
 
   return {
     statusCode,
     headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,GET"
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET",
     },
-    
+
     body: JSON.stringify(result, null, 2),
   };
-}
+};
--- product-service/src/getProductsList.ts
+++ product-service/src/getProductsList.ts
@@ -1,16 +1,60 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
-import productList from "../productList.json";
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import { Client } from "pg";
 
-export const getProductsList: APIGatewayProxyHandler = async (event, _context) => {
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false, // to avoid warring in this example
+  },
+  connectionTimeoutMillis: 5000, // time in millisecond for termination of the database query
+};
+
+export const getProductsList: APIGatewayProxyHandler = async (
+  event,
+  _context
+) => {
+  let statusCode = 404;
+  let result = {};
+  const client = new Client(dbOptions);
+  try {
+    await client.connect();
+
+    // make select query
+
+    const text =
+      "select p.*, s.count from products as p inner join stocks as s on p.id = s.product_id ";
+
+    const { rows: products } = await client.query(text);
+
+    statusCode = 200;
+    result = products;
+  } catch (err) {
+    // you can process error here. In this example just log it to console.
+    console.error("Error during database request executing:", err);
+
+    statusCode = 500;
+    result = {
+      message: "internal server error",
+    };
+  } finally {
+    // in case if error was occurred, connection will not close automatically
+    client.end(); // manual closing of connection
+  }
 
   return {
-    statusCode: 200,
+    statusCode,
     headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,GET"
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET",
     },
-    body: JSON.stringify(productList, null, 2),
+
+    body: JSON.stringify(result, null, 2),
   };
-}
+};
