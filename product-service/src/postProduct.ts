65a17d4c0201ccd992a4533a5eba73488614374a
Yakov Grischenko <coding@mail.ru>
2020-11-09 02:39:15 +0700

    feat: transaction support for add product

--- product-service/src/postProduct.ts
+++ product-service/src/postProduct.ts
@@ -2,18 +2,8 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 import { Client } from "pg";
 
-const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
-const dbOptions = {
-  host: PG_HOST,
-  port: PG_PORT,
-  database: PG_DATABASE,
-  user: PG_USERNAME,
-  password: PG_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false, // to avoid warring in this example
-  },
-  connectionTimeoutMillis: 5000, // time in millisecond for termination of the database query
-};
+import { dbOptions } from './dbOptions'
+
 
 export const postProduct: APIGatewayProxyHandler = async (event, _context) => {
   const { title, description = "", price = 0, count = 0 } = JSON.parse(
@@ -43,6 +33,8 @@ export const postProduct: APIGatewayProxyHandler = async (event, _context) => {
     try {
       await client.connect();
 
+      await client.query('BEGIN')
+
       const dmlResult = await client.query(
         `insert into products (title, description, price) values ($1, $2, $3) returning *`,
         [title, description, price]
@@ -55,6 +47,7 @@ export const postProduct: APIGatewayProxyHandler = async (event, _context) => {
         ($1, $2) returning *`,
         [newUUID, count]
       );
+      
 
       const {
         rows: [product],
@@ -62,13 +55,15 @@ export const postProduct: APIGatewayProxyHandler = async (event, _context) => {
         "select p.*, s.count from products as p inner join stocks as s on p.id = s.product_id where p.id = $1",
         [newUUID]
       );
+      
+      await client.query('COMMIT')
 
       statusCode = 201;
       result = product;
     } catch (err) {
       // you can process error here. In this example just log it to console.
       console.error("Error during database request executing:", err);
-
+      await client.query('ROLLBACK')
       statusCode = 500;
       result = {
         message: "internal server error",
