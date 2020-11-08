d2b00e5ab533e3fd956c33311acaa6bde593babe
Yakov Grischenko <coding@mail.ru>
2020-11-08 17:57:20 +0700

    feat: (TASK 4.1) sql for tables creation +  serverless-dotenv-plugin and pg installed

---
+++ product-service/db/ddl.sql
@@ -0,0 +1,28 @@
+create EXTENSION if not EXISTS "uuid-ossp";
+
+
+DROP TABLE IF exists products CASCADE; 
+
+create table products (
+	id uuid primary key default uuid_generate_v4(),
+	title text NOT NULL,
+	description text,
+	price integer
+);
+
+DROP TABLE IF exists stocks;
+
+create table stocks (
+	product_id uuid unique,
+	count integer,
+	foreign key ("product_id") references "products" ("id")
+);
+
+insert into c (id, title, price) values
+('33a36bf5-a146-406c-8655-e8977d8e0ab4', 'product2', 3),
+('33a36bf5-a146-406c-8655-e8977d8e0ab5', 'product3', 4);
+
+
+insert into stocks (product_id, count) values 
+('33a36bf5-a146-406c-8655-e8977d8e0ab4', 21),
+('33a36bf5-a146-406c-8655-e8977d8e0ab5', 22);
