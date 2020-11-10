7159a1e5dd3fff10b2fd24fe81a47386bc9b397b
Yakov Grischenko <coding@mail.ru>
2020-11-10 10:30:02 +0700

    upd: change initial products

--- product-service/db/ddl.sql
+++ product-service/db/ddl.sql
@@ -18,11 +18,4 @@ create table stocks (
 	foreign key ("product_id") references "products" ("id")
 );
 
-insert into products (id, title, price) values
-('33a36bf5-a146-406c-8655-e8977d8e0ab4', 'Счастье для всех даром v 2.0', 0),
-('33a36bf5-a146-406c-8655-e8977d8e0ab5', 'Товар с такой же картинкой', 99);
 
-
-insert into stocks (product_id, count) values 
-('33a36bf5-a146-406c-8655-e8977d8e0ab4', 7000000),
-('33a36bf5-a146-406c-8655-e8977d8e0ab5', 42);
