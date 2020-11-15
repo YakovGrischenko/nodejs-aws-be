0fbc9fb48cd5082e73aeac80d1fc5ab7e554b61e
Yakov Grischenko <coding@mail.ru>
2020-11-15 18:17:45 +0700

    feat: importFileParser implementation

--- import-service/src/importFileParser.ts
+++ import-service/src/importFileParser.ts
@@ -1,5 +1,6 @@
 import { S3Event, S3Handler } from "aws-lambda";
 import * as AWS from "aws-sdk";
+import * as csv from "csv-parser";
 import { BUCKET } from "./bucketOptions";
 import "source-map-support/register";
 
@@ -11,16 +12,42 @@ export const importFileParser: S3Handler = async (event: S3Event) => {
   for (const record of event.Records) {
     console.log("record.s3: ", record.s3);
 
-    await s3
-      .copyObject({
+    const s3Stream = s3
+      .getObject({
         Bucket: BUCKET,
-        CopySource: BUCKET + "/" + record.s3.object.key,
-        Key: record.s3.object.key.replace("uploaded", "parsed"),
+        Key: record.s3.object.key,
       })
-      .promise();
+      .createReadStream();
+    s3Stream
+      .pipe(csv())
+      .on("data", (data) => {
+        console.log(data);
+      })
+      .on("end", async () => {
+        console.log(
+          "moveing file  ",
+          BUCKET + "/" + record.s3.object.key,
+          "to /parsed folder"
+        );
+
+        await s3
+          .copyObject({
+            Bucket: BUCKET,
+            CopySource: BUCKET + "/" + record.s3.object.key,
+            Key: record.s3.object.key.replace("uploaded", "parsed"),
+          })
+          .promise();
+
+        await s3
+          .deleteObject({
+            Bucket: BUCKET,
+            Key: record.s3.object.key,
+          })
+          .promise();
+      });
   }
 
   return {
-    statusCode: 200,
+    statusCode: 202,
   };
 };
