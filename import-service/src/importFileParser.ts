3077edc16731328be56cf30709bf6943197e75a4
Yakov Grischenko <coding@mail.ru>
2020-11-18 00:21:18 +0700

    upd: additional try/catch in importFileParser lambda

--- import-service/src/importFileParser.ts
+++ import-service/src/importFileParser.ts
@@ -7,58 +7,66 @@ import 'source-map-support/register'
 export const importFileParser: S3Handler = async (event: S3Event) => {
   console.log('lambda : importFileParser, event: ', event)
 
-  const s3 = new AWS.S3({ region: 'eu-west-1' })
+  try {
+    const s3 = new AWS.S3({ region: 'eu-west-1' })
 
-  for (const record of event.Records) {
-    console.log('record.s3: ', record.s3)
+    for (const record of event.Records) {
+      console.log('record.s3: ', record.s3)
 
-    const s3Stream = s3
-      .getObject({
-        Bucket: BUCKET,
-        Key: record.s3.object.key
+      const s3Stream = s3
+        .getObject({
+          Bucket: BUCKET,
+          Key: record.s3.object.key
+        })
+        .createReadStream()
+
+      await new Promise((resolve, reject) => {
+        s3Stream
+          .pipe(csv())
+          .on('data', (data) => {
+            console.log(data)
+          })
+          .on('error', (error) => {
+            console.log('parsing error', error)
+            reject(error)
+          })
+          .on('end', async () => {
+            console.log(
+              'moving file  ',
+              BUCKET + '/' + record.s3.object.key,
+              'to /parsed folder'
+            )
+
+            await s3
+              .copyObject({
+                Bucket: BUCKET,
+                CopySource: BUCKET + '/' + record.s3.object.key,
+                Key: record.s3.object.key.replace('uploaded', 'parsed')
+              })
+              .promise()
+
+            console.log(
+              'removing file  ',
+              BUCKET + '/' + record.s3.object.key,
+              'to /parsed folder'
+            )
+
+            await s3
+              .deleteObject({
+                Bucket: BUCKET,
+                Key: record.s3.object.key
+              })
+              .promise()
+            resolve()
+          })
       })
-      .createReadStream()
-
-    await new Promise((resolve, reject) => {
-      s3Stream
-        .pipe(csv())
-        .on('data', (data) => {
-          console.log(data)
-        })
-        .on('error', (error) => {
-          console.log('parsing error', error)
-          reject(error)
-        })
-        .on('end', async () => {
-          console.log(
-            'moving file  ',
-            BUCKET + '/' + record.s3.object.key,
-            'to /parsed folder'
-          )
-
-          await s3
-            .copyObject({
-              Bucket: BUCKET,
-              CopySource: BUCKET + '/' + record.s3.object.key,
-              Key: record.s3.object.key.replace('uploaded', 'parsed')
-            })
-            .promise()
-
-          console.log(
-            'removing file  ',
-            BUCKET + '/' + record.s3.object.key,
-            'to /parsed folder'
-          )
-
-          await s3
-            .deleteObject({
-              Bucket: BUCKET,
-              Key: record.s3.object.key
-            })
-            .promise()
-          resolve()
-        })
-    })
+    }
+  } catch (error) {
+    console.error('lambds : importFileParser, error: ', error)
+    //i'm not sure about return code for s3 event. It could be return;
+    return {
+      statusCode: 500
+    }
   }
 
   return {
