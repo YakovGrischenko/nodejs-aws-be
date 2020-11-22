0e8768c50331b2eda931940c6e1f6f6afcabfc56
Yakov Grischenko <coding@mail.ru>
2020-11-22 15:48:39 +0700

    init: sns settings + base send email logic

--- product-service/src/catalogBatchProcess.ts
+++ product-service/src/catalogBatchProcess.ts
@@ -1,11 +1,36 @@
 import { SQSHandler, SQSEvent } from "aws-lambda";
+import * as AWS from "aws-sdk";
 import "source-map-support/register";
 
 export const catalogBatchProcess: SQSHandler = async (event: SQSEvent) => {
   console.log("Lambda function: catalogBatchProcess.  Event:", event);
+  const sns = new AWS.SNS({ region: "eu-west-1" });
 
   for (const record of event.Records) {
     console.log("sqs.record.body: ", record.body);
-    console.log("sqs.record.body parsed: ", JSON.parse(record.body));
+
+    let dataParsed = {};
+
+    try {
+      dataParsed = JSON.stringify(record.body);
+    } catch (error) {
+      console.error(`unable to parse message: ${record.body} error: ${error} `);
+    }
+
+    console.log("sqs.record.body parsed: ", dataParsed);
   }
+
+  await new Promise((resolve, reject) => {
+    sns.publish(
+      {
+        Subject: "notification subject",
+        Message: "new goods added",
+        TopicArn: process.env.SNS_ARN,
+      },
+      () => {
+        console.log("message sent to email");
+        resolve();
+      }
+    );
+  });
 };
